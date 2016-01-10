3454973c6a8a377cdb1454e47d204ec888837d5f
Chris Crowell <ccrowell18@gmail.com>
2016-01-10 04:42:46 -0500

    Add lodash to filter and check empty. Properly checks if room is available or not. Cron job runs every minute, takes less than a second to run.

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 var cron = require('cron-scheduler');
 var moment = require('moment');
 var pasync = require('pasync');
+var _ = require('lodash');
 var labSchedule = require('./labs');
 
-var frequency = '*/5 * * * *'; // Every 5 minutes
-var timezone = '';
+var cronObject = {
+	timezone: 'America/New_York',
+	on: '*/1 * * * *',
+	name: 'lab-update'
+};
 
-cron({
-	timezone: timezone,
-	on: frequency,
-	function() {
-
-	}
+cron(cronObject, function() {
+	pasync.each(labSchedule.labs, updateRooms);
 });
+
+function updateRooms(room) {
+	if(isRoomBusy(room)) {
+		console.log(room.room, 'red')
+	} else {
+		console.log(room.room, 'green')
+	}
+}
+
+function isRoomBusy(room) {
+	return !_.isEmpty(_.filter(room.busyTimes, isClassInSession));
+}
+
+function isClassInSession(time) {
+	if(isToday(time.dayOfWeek)) {
+		return classHasStarted(time.start) && !classHasEnded(time.end);
+	} else {
+		return false;
+	}
+}
+
+function isToday(day) {
+	return moment().isSame(moment().day(day), 'day');
+}
+
+function classHasStarted(startTime) {
+	return moment().isSameOrAfter(moment(startTime, 'HH:mm'));
+}
+
+function classHasEnded(endTime) {
+	return !moment().isSameOrBefore(moment(endTime, 'HH:mm'));
+}
+
+console.log('empty-lab started!');

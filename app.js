1444f97a718995974e69c84338b4518893f7194a
Chris Crowell <ccrowell18@gmail.com>
2016-01-10 03:23:24 -0500

    add moment and async dependencies

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var cron = require('cron-scheduler');
+var moment = require('moment');
+var pasync = require('pasync');
+var labSchedule = require('./labs');
 
 var frequency = '*/5 * * * *'; // Every 5 minutes
 var timezone = '';

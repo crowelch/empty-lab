c07c02c9a01bee10d17c35c34b3c172d566442d8
Chris Crowell <crowelch@mail.uc.edu>
2016-01-14 10:36:36 -0500

    Dynamically load in rooms.json on each page request, to get up to date data.

--- site.js
+++ site.js
@@ -1,17 +1,33 @@
+var fs = require('fs');
 var http = require('http');
 var Handlebars = require('handlebars');
 var cron = require('./cron');
-var rooms = require('./rooms');
-const PORT = 3000;
 
+const PORT = 3000;
 const source = '<table>{{#each rooms}}<tr><td>{{number}}</td><td>{{status}}</td></tr>{{/each}}</table>';
 
-var template = Handlebars.compile(source);
-var html = template(rooms);
+function getRooms(cb) {
+	fs.readFile('./rooms.json', 'utf-8', function(err, data) {
+		if(err) {
+			throw err;
+		}
+
+		getTemplate(JSON.parse(data), cb);
+	});
+}
+
+function getTemplate(rooms, cb) {
+	var template = Handlebars.compile(source);
+	var html = template(rooms);
+
+	cb(html);
+}
 
 function handleRequest(request, response) {
-	response.write(html);
-	response.end();
+	getRooms(function(html) {
+		response.write(html);
+		response.end();
+	});
 }
 
 var server = http.createServer(handleRequest);

4faf404260a86d5e4663933392c686cf28ca45e7
Chris Crowell <crowelch@mail.uc.edu>
2016-03-10 03:02:24 -0500

    Add comments and debug statements

--- pi.js
+++ pi.js
@@ -7,6 +7,7 @@ var utils = require('./utils');
 const HIGH = 1;
 const LOW = 0;
 
+//Object containing gpio pin, current value, and open status for the rooms
 var mapRoomToPin = {
 	'533': {
 		pin: 22,
@@ -30,24 +31,31 @@ var mapRoomToPin = {
 	}
 };
 
+// Settings for cron
 var cronObject = {
 	timezone: 'America/New_York',
 	on: '*/1 * * * *',
 	name: 'update-leds'
 };
 
+// Run cron job
 cron(cronObject, function() {
 	console.log('cron started', Date.now());
 
+	// Get latest room data
 	utils.getRooms().then(function(rooms) {
+		//TODO: Replace with async.each?
 		_.forEach(rooms, function(room) {
 			var roomData = mapRoomToPin[room.number];
 
 			console.log(roomData);
 
+			// Check status and call function to write to pins
 			if(room.status && (roomData.value !==	HIGH)) {
+				console.log('writing high to ' + roomData.pin);
 				writeToPin(roomData.pin, HIGH);
 			} else if(roomData.value !== LOW) {
+				console.log('writing low to ' + roomData.pin);
 				writeToPin(roomData.pin, LOW);
 			}
 		});
@@ -56,17 +64,21 @@ cron(cronObject, function() {
 	console.log('cron finished', Date.now());
 });
 
+// Write to gpio pins
 function writeToPin(pin, value) {
 	closePinIfOpen(roomData.pin, roomData.isOpen).then(function() {
+		console.log('Opening pin ' + pin);
 		gpio.open(pin, 'output', function(err) {
 			gpio.write(pin, value, function() {});
 		});
 	});
 }
 
+// Check if a pin is open, if so close it...?
 function closePinIfOpen(pin, isOpen) {
 	if(isOpen) {
 		gpio.close(pin);
+		console.log('closing pin ' + pin);
 	}
 
 	return Promise.resolve();
--- utils.js
+++ utils.js
@@ -1,26 +1,28 @@
 var fs = require('fs');
 
-var defaultRooms = {
-	rooms: [{
-		number: 533,
-		status: false
-	},{
-		number: 537,
-		status: false
-	},{
-		number: 548,
-		status: false
-	},{
-		number: 550,
-		status: false
-	}]
-}
+var defaultRooms={"rooms":[{"number":"533","status":false},{"number":"537","status":false},{"number":"548","status":false},{"number":"550","status":false}]};
+
+// var defaultRooms = {
+// 	rooms: [{
+// 		number: 533,
+// 		status: false
+// 	},{
+// 		number: 537,
+// 		status: false
+// 	},{
+// 		number: 548,
+// 		status: false
+// 	},{
+// 		number: 550,
+// 		status: false
+// 	}]
+// }
 
 exports.getRooms = function() {
 	return new Promise(function(resolve) {
 		fs.readFile('./rooms.json', 'utf-8', function(err, data) {
 			if(err) {
-				console.log(err);
+				console.log('error reading rooms.json: ' + err);
 				data = defaultRooms;
 			}
 

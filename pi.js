6516b5d815ff7be2e645c1f95920f485c68b100c
Chris Crowell <crowelch@mail.uc.edu>
2016-03-12 16:14:28 -0500

    testetstest

--- pi.js
+++ pi.js
@@ -191,8 +191,11 @@ function gpioWrite(pin, value) {
 
 // Save pin state
 function savePinState(pin, value) {
-	console.log(pin);
+	console.log('pin:' pin);
+	console.log(typeof pin);
+	console.log(typeof pins[pin]);
 	console.log(pins[pin]);
+
 	if(value === HIGH) {
 		pins[pin].isHigh = true;
 	} else {
